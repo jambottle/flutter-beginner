6a1145b144b873387112b874fdc00bb645e7e466
jambottle <jaewon.walks@gmail.com>
2022-09-17 01:00:16 +0900

    refactor: Separate HomePage widget from MyApp widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,63 +12,72 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Image.asset(
-            'assets/logo.png',
-            height: 32,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Image.asset(
+          'assets/logo.png',
+          height: 32,
+        ),
+        leading: IconButton(
+          icon: Icon(
+            CupertinoIcons.camera,
+            color: Colors.black,
           ),
-          leading: IconButton(
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
             icon: Icon(
-              CupertinoIcons.camera,
+              CupertinoIcons.paperplane,
               color: Colors.black,
             ),
             onPressed: () {},
           ),
-          actions: [
-            IconButton(
-              icon: Icon(
-                CupertinoIcons.paperplane,
-                color: Colors.black,
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(32),
+                child: Image.network(
+                  "https://i.ibb.co/CwzHq4z/trans-logo-512.png",
+                  width: 84,
+                ),
               ),
-              onPressed: () {},
-            ),
-          ],
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(32),
-                  child: Image.network(
-                    "https://i.ibb.co/CwzHq4z/trans-logo-512.png",
-                    width: 84,
-                  ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Enter your username',
                 ),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Enter your username',
-                  ),
+              ),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Enter your password',
                 ),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Enter your password',
-                  ),
+              ),
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.only(top: 16),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Submit'),
                 ),
-                Container(
-                  width: double.infinity,
-                  margin: const EdgeInsets.only(top: 16),
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Submit'),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
