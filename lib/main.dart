e274d49c6187c7628c0357c8a04619227ff05096
jambottle <jaewon.walks@gmail.com>
2022-09-17 02:35:50 +0900

    refactor: Separate HomePage widget from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'views/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,110 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        title: Image.asset(
-          'assets/logo.png',
-          height: 32,
-        ),
-        leading: IconButton(
-          icon: Icon(
-            CupertinoIcons.camera,
-            color: Colors.black,
-          ),
-          onPressed: () {},
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(
-              CupertinoIcons.paperplane,
-              color: Colors.black,
-            ),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          /* 사진 */
-          Image.network(
-            "https://cdn2.thecatapi.com/images/kat_7kqBi.png",
-            width: double.infinity,
-            height: 400,
-            fit: BoxFit.cover,
-          ),
-
-          /* 아이콘 목록 (좋아요, 댓글, 저장) */
-          Row(
-            children: [
-              IconButton(
-                icon: Icon(
-                  CupertinoIcons.heart,
-                  color: Colors.black,
-                ),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(
-                  CupertinoIcons.chat_bubble,
-                  color: Colors.black,
-                ),
-                onPressed: () {},
-              ),
-              Spacer(),
-              IconButton(
-                icon: Icon(
-                  CupertinoIcons.bookmark,
-                  color: Colors.black,
-                ),
-                onPressed: () {},
-              ),
-            ],
-          ),
-
-          /* 좋아요 수 */
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              "2 likes",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-
-          /* 설명 */
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              "My cat is docile even when bathed. I put a duck on his head in the wick and he's staring at me. Isn't it so cute? 🤭",
-            ),
-          ),
-
-          /* 날짜 */
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              "FEBURARY 6",
-              style: TextStyle(
-                color: Colors.grey,
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
