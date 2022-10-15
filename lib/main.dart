c36941bc27f9339d96f99f8e2580260d7ebe8bfc
jambottle <jaewon.walks@gmail.com>
2022-10-15 20:05:07 +0900

    feat: Handle foreground messages via the onMessage stream

--- lib/main.dart
+++ lib/main.dart
@@ -51,6 +51,15 @@ class _MyAppState extends State<MyApp> {
     if (!Platform.isAndroid) {
       appController.requestPermission();
     }
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
   }
 
   @override
